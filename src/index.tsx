3aebf4fee2299fe491ff670544f48acbec352526
iamogir <ia.mogirevsky@gmail.com>
2024-04-03 00:03:03 +0300

    added fonts (works!)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './general/redux/store';
 import App from './App';
 import './index.css';
+import './fonts/fonts.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
       <App />
-    </Provider>
 );
