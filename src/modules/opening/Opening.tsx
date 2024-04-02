1c24c83905012369b16d3fe5223ded6c1fcdb023
iamogir <ia.mogirevsky@gmail.com>
2024-04-03 00:05:41 +0300

    added css file for opening module

--- src/modules/opening/Opening.tsx
+++ src/modules/opening/Opening.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import background from "../../images/background.webp";
+import style from './opening.module.css';
 
 const Opening = () => {
     return (
         <div>
             <div>
                 <div>
-                    <h1>Khristina Ermak</h1>
+                    <h1 className={style.title}>Khristina Ermak</h1>
                     <p>Профессиональный репортажный и портретный фотограф. Индивидуальный подход, раскрытие Вас и вашей истории, чувств и настроения. Передача вашей личности через мой объектив.
                     </p>
                 </div>
